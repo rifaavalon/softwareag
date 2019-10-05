9d225688b9edb41a49ffa2472a56d209f73550bb
Chris Hickey <chrismhickey@gmail.com>
2019-10-04 19:15:43 -0600

    adding in the redirect to 8080 from 80

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -7,9 +7,18 @@ terraform {
     dynamodb_table = "terraform-state-lock"
   }
 }
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket         = "softwareag-remote-state-global"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
 
 provider "aws" {
-  region     = "us-west-2"
+  region = "us-west-2"
 }
 
 
@@ -32,5 +41,172 @@ module "vpc" {
   tags = {
     Terraform   = "true"
     Environment = "dev"
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+  # public_key = "${tls_private_key.auth.public_key_openssh}"
+}
+
+resource "aws_launch_configuration" "autoscale_launch" {
+  image_id        = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.sec_web.id}"]
+  key_name        = "${aws_key_pair.auth.id}"
+  #  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum -y update
+              sudo yum -y install nginx
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ebs_block_device {
+    device_name           = "/dev/sdh"
+    volume_type           = "gp2"
+    volume_size           = "30"
+    delete_on_termination = "true"
+  }
+}
+
+resource "aws_elb" "elb" {
+  name                      = "elb"
+  security_groups           = ["${aws_security_group.sec_lb.id}"]
+  subnets                   = "${module.vpc.public_subnets}"
+  cross_zone_load_balancing = true
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:80/"
+  }
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "80"
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_autoscaling_group" "autoscale_group" {
+  launch_configuration = "${aws_launch_configuration.autoscale_launch.id}"
+  vpc_zone_identifier  = "${module.vpc.private_subnets}"
+  load_balancers       = ["${aws_elb.elb.name}"]
+  min_size             = 3
+  max_size             = 3
+  tag {
+    key                 = "Name"
+    value               = "autoscale"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "sec_web" {
+  name        = "sec_web"
+  description = "Used for autoscale group"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "sec_lb" {
+  name   = "sec_elb"
+  vpc_id = "${module.vpc.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "alb" {
+  name            = "alb"
+  subnets         = "${module.vpc.public_subnets}"
+  security_groups = ["${aws_security_group.sec_lb.id}"]
+  internal        = false
+  idle_timeout    = 60
+  tags = {
+    Name = "alb"
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "alb-target-group"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = "${module.vpc.vpc_id}"
+  tags = {
+    name = "alb_target_group"
+  }
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 1800
+    enabled         = true
+  }
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 10
+    path                = "/"
+    port                = 80
+  }
+}
+
+resource "aws_autoscaling_attachment" "alb_autoscale" {
+  alb_target_group_arn   = "${aws_lb_target_group.alb_target_group.arn}"
+  autoscaling_group_name = "${aws_autoscaling_group.autoscale_group.id}"
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = "${aws_lb.alb.arn}"
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "8080"
+      protocol    = "HTTP"
+      status_code = "HTTP_301"
+    }
   }
 }
